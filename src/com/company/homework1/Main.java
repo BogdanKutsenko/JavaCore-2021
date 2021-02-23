358f1b7c6b2936405994b6d5498c31c7f45ed671
bgdnk <bgdnkutsenko@gmail.com>
2021-02-23 14:53:59 +0200

    added the second part of the homework

--- src/com/company/homework1/Main.java
+++ src/com/company/homework1/Main.java
@@ -1,10 +1,38 @@
 package com.company.homework1;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+    static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
 
         Bank bank = new Bank();
         bank.calculateProfit();
+        fillInArray1();
+        printArray1();
+        reversPrintArray1();
+
     }
+    static int array1 [] = new int[5];
+    public static void fillInArray1(){
+        System.out.println("Enter the numbers of array: ");
+        for (int i = 0; i < array1.length; i++) {
+            array1[i] = scanner.nextInt();
+        }
+    }
+    public static void printArray1(){
+        System.out.println("Your array is: ");
+        for (int i = 0; i < array1.length; i++) {
+            System.out.print(array1[i] + " ");
+        }
+        System.out.println();
+    }
+    public static void reversPrintArray1(){
+        System.out.println("Your array from end to the beginning is: ");
+        for (int i = array1.length - 1; i >= 0; i--) {
+            System.out.print(array1[i] + " ");
+        }
+        System.out.println();
+    }
+
 }
